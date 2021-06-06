cc8e4ea8fb5a0373789ae74d9e3c9bab0a97d78b
AmeerArena <85454475+AmeerArena@users.noreply.github.com>
2021-06-06 21:20:27 +0300

    Add files via upload

---
+++ RGGlobalItem.cs
@@ -0,0 +1,113 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.Utilities;
+using Terraria.ID;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+
+namespace ReloadableGuns
+{
+    public class RGGlobalItem : GlobalItem
+    {
+        public int AmmoNum;
+        public int AmmoMax;
+        public int AmmoMax2;
+        public override bool InstancePerEntity => true;
+
+        public override GlobalItem Clone(Item item, Item itemClone)
+        {
+            RGGlobalItem myClone = (RGGlobalItem)base.Clone(item, itemClone);
+            myClone.AmmoNum = AmmoNum;
+            myClone.AmmoMax = AmmoMax;
+            myClone.AmmoMax2 = AmmoMax2;
+            return myClone;
+        }
+        public override void SetDefaults(Item item)
+        {
+            if (item.ranged = true && item.useAmmo == AmmoID.Bullet)
+            {
+                AmmoNum = 40;
+                AmmoMax = 40;
+                AmmoMax2 = AmmoMax;
+                item.damage += (int)(item.damage * 0.12f);
+
+                if (item.type == ItemID.FlintlockPistol || item.type == ItemID.Musket || item.type == ItemID.TheUndertaker || item.type == ItemID.SniperRifle)
+                {
+                    AmmoNum = 7;
+                    AmmoMax = 7;
+                    AmmoMax2 = AmmoMax;
+                }
+                else if (item.type == ItemID.Boomstick || item.type == ItemID.Revolver || item.type == ItemID.Handgun)
+                {
+                    AmmoNum = 20;
+                    AmmoMax = 20;
+                    AmmoMax2 = AmmoMax;
+                }
+                else if (item.type == ItemID.Minishark || item.type == ItemID.Megashark)
+                {
+                    AmmoNum = 55;
+                    AmmoMax = 55;
+                    AmmoMax2 = AmmoMax;
+                }
+                else if (item.type == ItemID.Xenopopper || item.type == ItemID.VortexBeater)
+                {
+                    AmmoNum = 100;
+                    AmmoMax = 100;
+                    AmmoMax2 = AmmoMax;
+                }
+                else if (item.type == ItemID.ChainGun || item.type == ItemID.SDMG)
+                {
+                    AmmoNum = 200;
+                    AmmoMax = 200;
+                    AmmoMax2 = AmmoMax;
+                }
+                else if (item.useTime <= 4)
+                {
+                    AmmoNum = 110;
+                    AmmoMax = 110;
+                    AmmoMax2 = AmmoMax;
+                }
+                else if (item.useTime <= 7)
+                {
+                    AmmoNum = 75;
+                    AmmoMax = 75;
+                    AmmoMax2 = AmmoMax;
+                }
+            }
+        }
+        public override bool CanUseItem(Item item, Player player)
+        {
+            var modplayer = player.GetModPlayer<RGPlayer>();
+            if (item.ranged = true && item.useAmmo == AmmoID.Bullet)
+            {
+                if (AmmoNum <= 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    AmmoNum--;
+                    if (player.HasBuff(ModContent.BuffType<Buffs.FireingMomentBuff>()))
+                    {
+                        player.ClearBuff(ModContent.BuffType<Buffs.FireingMomentBuff>());
+                    }
+                    return true;
+                }
+            }
+            if (player.HeldItem.IsAir)
+            {
+                return true;
+            }
+            return base.CanUseItem(item, player);
+        }
+    }
+}
---
+++ RGPlayer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace ReloadableGuns
+{
+    public class RGPlayer : ModPlayer
+    {
+		public static RGPlayer ModPlayer(Player player)
+		{
+			return player.GetModPlayer<RGPlayer>();
+		}
+		public float AmmoAdd;
+		public float ReloadSpeed;
+		public bool CritBullet;
+		public override void ResetEffects()
+        {
+			Item item = player.HeldItem;
+			if (item.ranged = true && item.useAmmo == AmmoID.Bullet)
+			{
+				item.GetGlobalItem<RGGlobalItem>().AmmoMax2 = (int)(item.GetGlobalItem<RGGlobalItem>().AmmoMax * (1f + AmmoAdd));
+			}
+			ReloadSpeed = 0f;
+			AmmoAdd = 0;
+			CritBullet = false;
+		}
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+        {
+			Item item = player.HeldItem;
+            if (CritBullet == true && crit)
+            {
+				if (item.ranged = true && item.useAmmo == AmmoID.Bullet)
+                {
+					item.GetGlobalItem<RGGlobalItem>().AmmoNum += 1;
+				}
+            }
+        }
+        public override void DrawEffects(PlayerDrawInfo drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
+		{
+			if (player.HasBuff(ModContent.BuffType<Buffs.FireingMomentBuff>()))
+			{
+				for (int i = 0; i < 1; i++)
+				{
+					Vector2 vector2 = Vector2.UnitY.RotatedByRandom(6.28318548202515) * new Vector2((float)player.height, (float)player.height) * 0.4f / 2f;
+					int index = Dust.NewDust(player.Center + vector2, 0, 0, DustID.GoldCoin, 0.0f, 0.0f, 240, new Color(), 0.05f);
+					Main.dust[index].position = player.Center + vector2;
+					Main.dust[index].velocity = Vector2.Zero;
+					Main.dust[index].noGravity = true;
+				}
+			}
+		}
+		public override void ProcessTriggers(TriggersSet triggersSet)
+		{
+			if (ReloadableGuns.ReloadGun.JustPressed)
+			{
+				int time = (int)(60 * (1f - ReloadSpeed));
+				if (time <= 6)
+                {
+					time = 6;
+                }
+				player.AddBuff(ModContent.BuffType<Buffs.ReloadBuff>(), time);
+			}
+		}
+    }
+}
