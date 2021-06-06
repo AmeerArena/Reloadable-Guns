02431acf98967014e998a2d62a402dfd6f129cd0
AmeerArena <85454475+AmeerArena@users.noreply.github.com>
2021-06-06 21:28:31 +0300

    Add files via upload

---
+++ Buffs/ReloadBuff.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ReloadableGuns.Buffs
+{
+    public class ReloadBuff : ModBuff
+    {
+        public int Timer;
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Reloading");
+            Description.SetDefault("");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+        public int SP = 5;
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.delayUseItem = true;
+            SP--;
+            if (SP <= 0)
+            {
+                SP = 5;
+            }
+            Timer++;
+            if (Timer >= 18)
+            {
+                Main.PlaySound(2, (int)player.position.X, (int)player.position.Y, 108);
+                Timer = 0;
+            }
+
+            if (player.buffTime[buffIndex] <= 2)
+            {
+                Item item = player.HeldItem;
+                var modPlayer = Main.LocalPlayer.GetModPlayer<RGPlayer>();
+                item.GetGlobalItem<RGGlobalItem>().AmmoNum = item.GetGlobalItem<RGGlobalItem>().AmmoMax2;
+                player.AddBuff(ModContent.BuffType<FireingMomentBuff>(), 360);
+            }
+        }
+    }
+}
